5c071734e845cc796ba2c926a836246c82d806e0
Kavitha KC <kavithaprajod@gmail.com>
2022-08-13 14:46:25 +0800

    Created seed for listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,5 +2,5 @@ class Listing < ApplicationRecord
   validates :title, presence: true, length: { minimum: 6 }
   validates :description, :country, presence: true
   validates :address, presence: true, uniqueness: true
-  validates :bedroom_count, :bathroom_count, :bed_count, presence: true, numericality: { only_integer: true }
+  validates :bedroom_count, :bathroom_count, :bed_count, :guest_count, presence: true, numericality: { only_integer: true }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_13_024952) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_13_054451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "listings", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "country"
+    t.string "address"
+    t.float "price_per_night"
+    t.integer "bedroom_count"
+    t.integer "bathroom_count"
+    t.integer "guest_count"
+    t.integer "bed_count"
+    t.text "house_rules"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
