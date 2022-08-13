65a4d828c9f4efc2908fc4bd0dcf47233a29f657
Verina Ong Dinata <verina.od1997@gmail.com>
2022-08-13 23:05:09 +0800

    add scss for booking index page

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :listings
-  resources :bookings, only: %i[index show]
+  resources :bookings, except: %i[delete destroy]
 end
