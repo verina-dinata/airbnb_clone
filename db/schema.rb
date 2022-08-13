415ecfbfcf4a11c570dbee86fcd9f9002e582d59
Jonathan Tay <jonathan.taysc@gmail.com>
2022-08-13 16:04:59 +0800

    update migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_13_071709) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_13_080052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_13_071709) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
