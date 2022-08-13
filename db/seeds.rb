3ed2bdc63e17692a663d9b51293dd876955e8197
Jonathan Tay <jonathan.taysc@gmail.com>
2022-08-13 14:46:54 +0800

    Update Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating 5 Faker users (testX@gmail.com) (PW:Abc123!!")
+5.times do |i|
+  user =  User.create!(
+    first_name: Faker::Name.first_name
+    last_name: Faker::Name.last_name
+    email: "test#{i}@gmail.com"
+    password: "Abc123!!"
+    phone_number: Faker::PhoneNumber
+  )
+puts "Created users test1@gmail.com to test5@gmail.com"
