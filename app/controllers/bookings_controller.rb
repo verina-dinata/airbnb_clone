28b46eb19b71a3119dd7fe671382a17949704c3b
Verina Ong Dinata <verina.od1997@gmail.com>
2022-08-14 11:43:01 +0800

    Create a booking show view page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,19 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show]
+
   def index
     @bookings = Booking.all
+    @past_bookings = @bookings.select { |booking| booking.end_date < Date.today }
+    @upcoming_bookings = @bookings.select { |booking| booking.end_date > Date.today }
+  end
+
+  def show
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+
   end
 end
