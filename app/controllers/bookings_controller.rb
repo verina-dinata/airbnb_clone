54a0153d50f379ab998fce2c46c961bba3503bab
Verina Ong Dinata <verina.od1997@gmail.com>
2022-08-14 23:29:32 +0800

    Remove empty line

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,7 +5,6 @@ class BookingsController < ApplicationController
     @bookings = Booking.all
     @past_bookings = @bookings.select { |booking| booking.end_date < Date.today }
     @upcoming_bookings = @bookings.select { |booking| booking.end_date > Date.today }
-
   end
 
   def show
